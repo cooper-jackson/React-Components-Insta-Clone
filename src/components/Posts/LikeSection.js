258cbd4bed06f357655791b1eede50fec2cc9f1f
Cooper Jackson <davisjaxon@gmail.com>
2021-04-13 17:08:32 -0700

    finished assignment

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -10,6 +10,8 @@ const LikeSection = props => {
   // 🔥 Make sure the parent of LikeSection is passing the right props!
   const { likePost, numberOfLikes } = props;
 
+  // setIcon()
+
   return (
     <div>
       <div
@@ -17,13 +19,13 @@ const LikeSection = props => {
         key='likes-icons-container'
       >
         <div className='like-section-wrapper'>
-          <FontAwesomeIcon icon={faHeart} />
+          <FontAwesomeIcon id="heart" icon={faHeart} onClick={likePost}/>
         </div>
         <div className='like-section-wrapper'>
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className='like-number'>100 likes</p>
+      <p className='like-number'>{numberOfLikes} likes</p>
     </div>
   );
 };
